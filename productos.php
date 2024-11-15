5a909966aa941d703743cc6ca8505b4894ca30c3
Ariadne Zinser <ariadnezinser@gmail.com>
2024-11-15 15:54:08 -0600

    Inicio de proyecto y navegación básica

---
+++ productos.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coque - Productos</title>
+      <!-- Latest compiled and minified CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- Latest compiled JavaScript -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script> 
+</head>
+<body>
+    <!-- Contenedor principal de BS5 -->
+     <div class="container-fluid">
+        <nav class="navbar navbar-expand-sm bg-danger navbar-danger">
+            <div class="container-fluid">
+              <ul class="navbar-nav">
+                <li>
+                    <a class="navbar-brand" href="index.html">
+                        <img src="logo_insatagram.jpg.png" alt="Insatagram Logo" style="width:40px;" class="rounded-pill"> 
+                    </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="index.html">Inicio</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link active" href="productos.php">Productos</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="consulta.php">Consulta</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="about.html">Acerca de</a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        <div class="mt-4 p-5 bg-light text-white rounded">
+            <h1 class="text-danger">C O Q U É</h1>
+        </div>
+        <div class="container">
+            <h2 class="my-5">Red social cool.</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt similique quibusdam id enim iste exercitationem sapiente error saepe aliquid amet. Dolorem provident et voluptate pariatur. Corrupti aspernatur necessitatibus distinctio soluta.</p>
+            <p>Possimus ipsam vero, iure ullam vitae magnam quidem omnis voluptatum cumque harum aut neque pariatur distinctio explicabo eveniet aliquam officia maiores iusto tenetur voluptas, voluptates eligendi dicta assumenda? Numquam, modi?</p>
+            <p>Sunt quo recusandae at perferendis obcaecati error, omnis iusto optio nisi consequuntur quibusdam deleniti soluta maxime aut beatae, doloribus ad autem modi iste ea? Aliquam consequuntur velit explicabo voluptatum voluptate.</p>
+            <p>Corporis perferendis consequuntur, labore quisquam explicabo recusandae asperiores ipsa nulla numquam quia facilis fuga quasi fugit officiis similique ullam modi nostrum illum rem sunt. Provident dolorum distinctio delectus velit a?</p>
+            <p>Possimus obcaecati accusantium ipsum ab, ducimus quisquam molestiae iusto debitis incidunt. Optio natus voluptas rem voluptatem ut nihil dolorem maiores veniam deserunt sint quaerat dicta sequi, beatae quidem assumenda recusandae.</p>
+        </div>
+     </div>
+    
+</body>
+</html>
