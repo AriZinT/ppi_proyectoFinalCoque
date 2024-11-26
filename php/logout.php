16d020e6f5765b8d873aa7fe5bff65d8195abc8c
Ariadne Zinser <ariadnezinser@gmail.com>
2024-11-26 12:22:55 -0600

    Logout: barra navegación agregada

--- php/logout.php
+++ php/logout.php
@@ -10,6 +10,51 @@ session_start(); // Inicia la sesión
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
+     <!-- Contenedor principal de BS5 -->
+     <div class="container-fluid">
+        <nav class="navbar navbar-expand-sm bg-light navbar-light">
+            <div class="container-fluid">
+              <!-- <a class="navbar-brand" href="#">Logo</a> -->
+              <img src="../img/coque.jpg" alt="Coque Logo" style="width:40px;" class="rounded-pill">
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                <ul class="navbar-nav">
+                  <li class="nav-item">
+                    <a class="nav-link text-danger active" href="../index.php">Página Principal</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link text-danger" href="productos.php">Productos</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link text-danger" href="carrito.php">Carrito</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link text-danger" href="historial.php">Tu Historial</a>
+                  </li>
+                  <li class="nav-item"><a class="nav-link text-danger" href="perfil_usuario.php">Tu Perfil
+                    </a></li>
+                  <li class="nav-item">
+                    <a class="nav-link text-danger" href="nueva_cuenta.php">Nueva Cuenta</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link text-danger" href="admin.php">Administración</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link text-danger" href="../about.php">Acerca de Nosotros</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link text-danger" href="./logout.php"> Iniciar Sesión</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav> 
+        <div class="mt-4 p-5 bg-light text-white rounded">
+            <h1 class="text-danger">C O Q U É</h1>
+        </div>
+     </div>
     <div class="container mt-5">
         <h2 class="text-center">Sesión Cerrada</h2>
         <?php
